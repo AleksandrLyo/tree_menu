6a8388a05683632f7c93315025033d2ec29dc528
AleksandrLyo <al.lyo@yandex.ru>
2023-01-12 16:51:01 +0300

    Модели меню

--- tree_menu/menu/models.py
+++ tree_menu/menu/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(
+        max_length=64,
+        verbose_name='Menu name'
+    )
+    slug = models.SlugField(
+        max_length=64,
+        unique=True,
+        verbose_name="Menu slug"
+    )
+
+    class Meta:
+        verbose_name = 'Menu'
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(
+        max_length=64,
+        verbose_name='Item name'
+    )
+    slug = models.SlugField(
+        max_length=64,
+        verbose_name="Item slug"
+    )
+    menu = models.ForeignKey(
+        Menu,
+        blank=True,
+        null=True,
+        related_name='items',
+        on_delete=models.CASCADE,
+        verbose_name="Menu"
+    )
+    parent = models.ForeignKey(
+        'Item',
+        blank=True,
+        null=True,
+        related_name='childrens',
+        on_delete=models.CASCADE,
+        verbose_name="Parent"
+    )
+
+    class Meta:
+        verbose_name = 'Menu item'
+
+    def __str__(self):
+        return self.name
