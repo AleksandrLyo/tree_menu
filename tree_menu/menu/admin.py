cbbecfdaee00c38d6b16f854129e2f1b6cba1598
AleksandrLyo <al.lyo@yandex.ru>
2023-01-14 16:19:09 +0300

    Добавлены templatetags, html-страницы для примера

--- tree_menu/menu/admin.py
+++ tree_menu/menu/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Menu, Item
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent')
+    search_fields = ('name', )
+    list_filter = ('name',)
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name', )
+    list_filter = ('name',)
